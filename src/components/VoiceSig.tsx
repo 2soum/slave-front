c2c1abb5b84db26114fe5ba0451e805acdefff13
Nam <namluan.guerin@gmail.com>
2024-12-12 15:24:31 +0100

    update button

--- src/components/VoiceSig.tsx
+++ src/components/VoiceSig.tsx
@@ -35,6 +35,10 @@ const VoiceSig: React.FC<VoiceSigProps> = ({ updateColor, updateIntensity, updat
   const animationFrameRef = useRef<number | null>(null);
   const freqsRef = useRef<Uint8Array | null>(null);
   const audioChunks = useRef<Blob[]>([]);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);  // État pour savoir si l'audio est en lecture ou en pause
+  const audioRef = useRef<HTMLAudioElement | null>(null);  // Référence pour l'élément audio
+
+
 
   const opts: VisualizerOptions = {
     smoothing: 0.8,
@@ -114,6 +118,30 @@ const VoiceSig: React.FC<VoiceSigProps> = ({ updateColor, updateIntensity, updat
           console.error("Error sending audio to API:", error);
         }
       };
+    }
+  };
+
+  const playAudio = (): void => {
+    if (audioURL) {
+      const audio = new Audio(audioURL);
+      audioRef.current = audio;
+      audio.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const pauseAudio = (): void => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const toggleAudio = (): void => {
+    if (isPlaying) {
+      pauseAudio();
+    } else {
+      playAudio();
     }
   };
 
@@ -212,87 +240,110 @@ const VoiceSig: React.FC<VoiceSigProps> = ({ updateColor, updateIntensity, updat
     <div className="fixed z-50 bottom-0 left-0 right-0 flex justify-center pb-8">
       <div className="flex justify-center items-center">
         {!isRecording ? (
-          <div className="relative group">
-            <button
-              onClick={startRecording}
-              className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
-            >
+            <div className="relative group">
+              <button
+                  onClick={startRecording}
+                  className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
+              >
               <span
-                className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+                  className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
               ></span>
-              <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
+                <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
                 <div className="relative z-10 flex items-center space-x-2">
                   <span className="transition-all duration-500 group-hover:translate-x-1">
                     Start Recording
                   </span>
                   <svg
-                    className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      clipRule="evenodd"
-                      d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                      fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                        fillRule="evenodd"
                     ></path>
                   </svg>
                 </div>
               </span>
-            </button>
-          </div>
+              </button>
+            </div>
         ) : (
-          <div className="relative group">
-            <button
-              onClick={stopRecording}
-              className="relative inline-block p-px font-semibold leading-6 text-white bg-red-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
-            >
+            <div className="relative group">
+              <button
+                  onClick={stopRecording}
+                  className="relative inline-block p-px font-semibold leading-6 text-white bg-red-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
+              >
               <span
-                className="absolute inset-0 rounded-xl bg-gradient-to-r from-red-400 via-pink-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+                  className="absolute inset-0 rounded-xl bg-gradient-to-r from-red-400 via-pink-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
               ></span>
-              <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
+                <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
                 <div className="relative z-10 flex items-center space-x-2">
                   <span className="transition-all duration-500 group-hover:translate-x-1">
                     Stop Recording
                   </span>
                   <svg
-                    className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      clipRule="evenodd"
-                      d="M5 5a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-.75.75h-8.5A.75.75 0 0 1 5 13.5V5Z"
-                      fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5 5a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-.75.75h-8.5A.75.75 0 0 1 5 13.5V5Z"
+                        fillRule="evenodd"
                     ></path>
                   </svg>
                 </div>
               </span>
-            </button>
-            <canvas ref={canvasRef} className="w-64 h-32" />
-          </div>
+              </button>
+              <canvas ref={canvasRef} className="w-64 h-32"/>
+            </div>
         )}
-        {audioURL && (
-            <audio controls className="mt-4">
-              <source src={audioURL} type="audio/webm"/>
-              Your browser does not support the audio element.
-            </audio>
-          // <div className="flex flex-col items-center">
-          //   <audio controls src={audioURL} className="w-full" />
-          //   <button
-          //       onClick={() => {
-          //         const audio = new Audio(audioURL);
-          //         audio.play();
-          //       }}
-          //       className="mt-2 bg-green-600 text-white px-4 py-2 rounded"
-          //   >
-          //     Replay Recording
-          //   </button>
-          // </div>
-          )}
+        <div>
+          <div
+              className="fixed bottom-6 left-6 flex items-center justify-center w-14 h-14 bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 hover:shadow-2xl hover:shadow-blue-300 transition-all duration-300">
+            <button onClick={toggleAudio} className="w-full h-full flex items-center justify-center">
+              {/* Affichage de l'icône */}
+              <svg
+                  className="w-7 h-7 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+              >
+                {isPlaying ? (
+                    // Icône Pause (deux barres)
+                    <path d="M6 4H8V16H6zM12 4H14V16H12z"/>
+                ) : (
+                    // Icône Play (triangle)
+                    <path d="M6 4.5V15.5a.5.5 0 0 0 .75.43l8.75-5.75a.5.5 0 0 0 0-.86L6.75 4.07A.5.5 0 0 0 6 4.5z"/>
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+        {/*{audioURL && (*/}
+        {/*    <button*/}
+        {/*        onClick={playAudio}*/}
+        {/*        className="fixed bottom-8 left-8 flex items-center justify-center w-16 h-16 bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 hover:shadow-2xl hover:shadow-blue-300 transition-all duration-300"*/}
+        {/*    >*/}
+        {/*      <svg*/}
+        {/*          className="w-10 h-10 text-white"*/}
+        {/*          xmlns="http://www.w3.org/2000/svg"*/}
+        {/*          viewBox="0 0 20 20"*/}
+        {/*          fill="currentColor"*/}
+        {/*          aria-hidden="true"*/}
+        {/*      >*/}
+        {/*        <path*/}
+        {/*            d="M6 4.5V15.5a.5.5 0 0 0 .75.43l8.75-5.75a.5.5 0 0 0 0-.86L6.75 4.07A.5.5 0 0 0 6 4.5z"*/}
+        {/*        />*/}
+        {/*      </svg>*/}
+        {/*    </button>*/}
+        {/*)}*/}
       </div>
     </div>
   );
