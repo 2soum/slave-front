7fe222ced847f832ad2fc1a1bbacaba1112a748a
2soum <2soum.pro@gmail.com>
2024-12-12 11:08:19 +0100

    Fix roadmap

--- src/pages/Sprint.tsx
+++ src/pages/Sprint.tsx
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 import AnimatedFace from '../components/AnimatedFace';
 
 const Sprint = () => {
+  const color = "#00BFFF"; // Define the color variable in a blue cyan hexa
+  const intensity = 0.5; // Define the intensity variable
   return (
       <div className="min-h-screen">
         <Header />
-        <AnimatedFace intensity={0} />
+        <AnimatedFace color={color} intensity={intensity} />
         <Sprints />
       </div>
   );
